699325b99cc8f405c3c7cf3f862fcbb39f352547
joabk <mjoab88@gmail.com>
2020-01-25 09:37:37 +0300

    Add custom validation

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -1,7 +1,10 @@
 import { AbstractControl,ValidationErrors } from '@angular/forms';
 
 export class UsernameValidators{
-  //cannotContainSpaces(control: AbstractControl): ValidationErrors | null{
-
+  cannotContainSpaces(control: AbstractControl): ValidationErrors | null{
+    let value: string = control.value;
+    if(value.indexOf(' ')>=0){
+      return {cannotContainSpaces: true}
+    }
   }
 }
