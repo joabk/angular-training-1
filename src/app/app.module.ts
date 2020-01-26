edc06e9e42b871074cb180744a4438ac2f006abf
joabk <mjoab88@gmail.com>
2020-01-26 11:07:19 +0300

    Added http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SignupFormComponent  } from './signup-form/signup-form.component';
-import { NewCoursesFormComponent } from './new-courses-form/new-courses-form.component'
+import { NewCoursesFormComponent } from './new-courses-form/new-courses-form.component';
+import { PostsComponent } from './posts/posts.component'
 
 @NgModule({
   imports: [ 
     BrowserModule, 
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
   declarations: [ 
     AppComponent, 
     HelloComponent,
     SignupFormComponent,
-    NewCoursesFormComponent
+    NewCoursesFormComponent,
+    PostsComponent
     ],
   bootstrap: [ AppComponent ]
 })
---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+  posts:any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http:Http) {
+
+   }
+
+  ngOnInit() {
+    this.http.get(this.url)
+      .subscribe((response)=>{
+        this.posts = response.json();
+        //console.log(this.posts);
+      })
+  }
+
+  createPost(input: HTMLInputElement){
+    let post = { title:input.value}
+    input.value = '';
+
+    this.http.post(this.url,JSON.stringify(post))
+      .subscribe((response)=>{
+        post['id'] = response.json().id;
+        this.posts.splice(0,0,post);
+      });
+  }
+
+  updatePost(post){
+    this.http.patch(this.url + '/' + post.id ,JSON.stringify(post))
+      .subscribe((response)=>{
+        console.log(response);
+      })
+  }
+
+  deletePost(post){
+    this.http.delete(this.url + '/' + post.id)
+      .subscribe((response)=>{
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index,1);
+      })
+  }
+
+}
