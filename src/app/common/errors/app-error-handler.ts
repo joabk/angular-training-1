3c0adbd85a57b0ef939319a4e4da33098095aeb3
joabk <mjoab88@gmail.com>
2020-01-28 08:37:03 +0300

    Corrected error handler

--- src/app/common/errors/app-error-handler.ts
+++ src/app/common/errors/app-error-handler.ts
@@ -1,11 +1,11 @@
 import { ErrorHandler } from '@angular/core';
-import { throwError } from 'rxjs';
+
 import { AppError } from './app-error'
 
 export class AppErrorHandler implements ErrorHandler{
   //constructor(public error:){}
   handleError(error){
-    /alert('An unexpected error occurred');
+    alert('An unexpected error occurred');
     console.log('Error handled by app error handler ',error);
   }
 }
