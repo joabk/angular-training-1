8e3df22ece681ec741863aa0f3ae03308cb24225
joabk <mjoab88@gmail.com>
2020-01-28 11:50:14 +0300

    2 files modified

--- src/app/common/errors/app-error-handler.ts
+++ src/app/common/errors/app-error-handler.ts
@@ -4,7 +4,7 @@ import { AppError } from './app-error'
 export class AppErrorHandler implements ErrorHandler{
   //constructor(public error:){}
   handleError(error){
-    alert('An unexpected error occurred');;
+    //alert('An unexpected error occurred');;
     console.log('Error handled by app error handler ',error);
   }
 }
